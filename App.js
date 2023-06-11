5a887092e5c1da229e32832539aae382018cb27b
Leticia Minelvino <leticiaminelvino@gmail.com>
2023-06-11 14:51:22 -0400

    modificou fonte

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { StatusBar, SafeAreaView } from 'react-native';
-//SafeAreaView para o ios para evitar a barra de status
+import { StatusBar, SafeAreaView, View } from 'react-native'; //SafeAreaView para o ios para evitar a barra de status
+import {
+  useFonts, 
+  Montserrat_400Regular,
+  Montserrat_700Bold
+} from '@expo-google-fonts/montserrat';
 
 import Cesta from "./src/telas/Cesta" //nao precisa dos cochetes pq ja exportamos no outro arquivo
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+  if(!fonteCarregada){
+    return <View/>
+  }
   return (
     <SafeAreaView>
       <StatusBar/>
