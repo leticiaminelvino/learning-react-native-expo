410f615586b6cd1a9bca3448568a17bdcb94f8f4
Leticia Minelvino <leticiaminelvino@gmail.com>
2023-06-10 23:09:24 -0400

    title added and styled

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,6 @@
 
 import React from 'react'; //sempre exportar React from react
-import { Image, StyleSheet, Dimensions } from 'react-native';//sempre q usamos um componente temos que importar:
+import { Image, StyleSheet, Dimensions, Text } from 'react-native';//sempre q usamos um componente temos que importar:
 
 import topo from "../../assets/topo.png";
 
@@ -8,12 +8,26 @@ const width = Dimensions.get('screen').width; //para pegar a largura da tela do 
 
 export default function Cesta(){ 
     //default para que so tenha um componente
-    return <Image source={topo} style={estilos.topo}/>
+    return <>
+    <Image source={topo} style={estilos.topo}/>
+    <Text style={estilos.titulo}>Detalhes da Cesta</Text>
+    </>
 };
 
 const estilos = StyleSheet.create({
     topo:{
         width: "100%",
-        height: ((578/768) * width), //largura da img divide largura da img multipicla pelo width
+        height: ((578/768) * width), //largura da img divide largura da img multipicla pelo width   
+    },
+    titulo: {
+        width:"100%", //centro da tela
+        position: "absolute", //joga por cima da img
+        textAlign: "center",
+        fontSize: 16,
+        lineHeight:26,
+        color:"white",
+        fontWeight:"bold",
+        padding:16
+
     }
 });
