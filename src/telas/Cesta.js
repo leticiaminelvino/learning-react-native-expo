8714d22b984d4ee21b27073e285039dbf4fe9f80
Leticia Minelvino <leticiaminelvino@gmail.com>
2023-06-10 22:08:31 -0400

    initial commit

---
+++ src/telas/Cesta.js
@@ -0,0 +1,8 @@
+
+import React from 'react'; //sempre exportar React from react
+import { Text } from 'react-native';//sempre q usamos um componente temos que importar:
+
+//default para que so tenha um componente
+export default function Cesta(){
+    return <Text>Cesta</Text>
+}
