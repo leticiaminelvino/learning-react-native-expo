1b8d7e34ba2e4523637087b0059368d94dc36a03
letícia stem <lmdc.eng19@uea.edu.br>
2023-06-17 11:10:08 -0400

    terminar

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,5 +1,11 @@
 import logo from "../../assets/logo.png";
 
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png';
+
 const cesta = {
     topo:{
         titulo:"Detalhe da cesta"
@@ -12,7 +18,31 @@ const cesta = {
         preco: "R$ 40,00",
         botao: "Comprar",
     },
-
+    itens:{
+        titulo: "Itens da cesta",
+        lista: [
+            {
+                nome:"Tomate",
+                imagem: tomate,
+            },
+            {
+                nome:"Brocolis",
+                imagem: brocolis,
+            },
+            {
+                nome: "Batata",
+                imagem:batata,
+            },
+            {
+                nome: "Pepino",
+                imagem:pepino,
+            },
+            {
+                nome: "Abóbora",
+                imagem: abobora,
+            }
+        ]
+    }
 }
 
 export default cesta;
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -4,14 +4,16 @@ import { StyleSheet, View } from 'react-native';//sempre q usamos um componente 
 
 import Topo from './componentes/Topo';
 import Detalhes from './componentes/Detalhes';
+import Itens from './componentes/Itens'
 
-export default function Cesta({topo, detalhes}){ 
+export default function Cesta({topo, detalhes, itens}){ 
     //default para que so tenha um componente
     return <>
         <Topo {...topo}/>
 
         <View style={estilos.cesta}> 
             <Detalhes {...detalhes}/>
+            <Itens {...itens}/>
         </View>
     </>
 };
