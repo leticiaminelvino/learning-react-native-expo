aac9068c22e1956febbbcfd2eefb614e310e2661
letícia stem <lmdc.eng19@uea.edu.br>
2023-06-17 11:10:33 -0400

    terminar

---
+++ src/telas/Cesta/componentes/Itens.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Texto from '../../../componentes/Texto'
+
+export default function Itens({titulo, lista}){
+    return <>
+    <Texto>  {titulo} </Texto>
+    </>
+}
