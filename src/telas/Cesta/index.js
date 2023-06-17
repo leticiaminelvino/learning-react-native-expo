95490da79031a1beae8381ddf7be2acc81c7905b
letícia stem <lmdc.eng19@uea.edu.br>
2023-06-17 12:18:52 -0400

    exibir listas com .map()

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,6 +1,6 @@
 
 import React from 'react'; //sempre exportar React from react
-import { StyleSheet, View } from 'react-native';//sempre q usamos um componente temos que importar:
+import { ScrollView, StyleSheet, View } from 'react-native';//sempre q usamos um componente temos que importar:
 
 import Topo from './componentes/Topo';
 import Detalhes from './componentes/Detalhes';
@@ -8,14 +8,14 @@ import Itens from './componentes/Itens'
 
 export default function Cesta({topo, detalhes, itens}){ 
     //default para que so tenha um componente
-    return <>
+    return <ScrollView>
         <Topo {...topo}/>
 
         <View style={estilos.cesta}> 
             <Detalhes {...detalhes}/>
             <Itens {...itens}/>
         </View>
-    </>
+    </ScrollView>
 };
 
 const estilos = StyleSheet.create({
